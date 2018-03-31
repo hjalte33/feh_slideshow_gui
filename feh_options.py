a35f5b4cdacc98f29435e74ea61ac448fc192cf3
Hjalte Nielsen <hjalte33@gmail.com>
2018-03-31 22:23:26 +0200

    Adde loads of commens to the code.

--- feh_options.py
+++ feh_options.py
@@ -1,8 +1,14 @@
 #This class holds options for the slideshow 
 from tkinter import BooleanVar
 
+# TODO 
 
-class FehOptions(object):
+class FehOptions():
+    """
+    Class that contains feh options that can be manipulated using the
+    set and get functions.
+    """
+
     def __init__(self):
         self.options = {}
         self._delay = 7
@@ -30,25 +36,55 @@ class FehOptions(object):
         self.options['delay'] = {'lang_name':'forsinkelse', 'feh_arg':lambda:'--slideshow-delay,'+ str(self._delay)}
         
     def get_feh_args (self):
+        """
+        Returns a comma seperated string of activated feh arguments
+        """
+
+        # Prepare an output string 
         output = ''
-        flag = False
+
+        # Go throug each option element
         for val in self.options.values():
-            if 'activated' in val and val['activated'].get() == True:  # using short circuit evaluation to first check if activated is even in the list. 
+
+            # Using short circuit evaluation to see if the activated keys exists,
+            # then check if the option is activeted
+            if 'activated' in val and val['activated'].get() == True: 
+
+                # Add the option the output string and add a comma
                 output += (val['feh_arg'])
                 output += ','
+        
+        # Lastly add the delay option seperatly because it contains a lambda function
         output += self.options['delay']['feh_arg']()      
+
+        # Return the output string
         return(output)
 
     def set_option (self,name,data):
+        """Set a feh option given a name and the data
+        
+        Arguments:
+            name {str} -- [description]
+            data {bool/float} -- data type of the given option name. 
+                                Bool for on/off options, float for delay option
+        """
+
+        # Is the changing option the delay
         if name == 'delay': 
             try:
+                # Try to convert the data to a float
                 self._delay = float(data)
-                print(self._delay)
             except ValueError:
+                # Give error message and return if data conversion failed.
                 print('set option delay went wrong')
                 return
+        
+        # Else if the data type is a bool
         elif isinstance(data,bool):
             try:
+                # Try to set the given option name 
                 self.options[name]['activated'].set(str(data))
+
+            # If given a wrong option return
             except KeyError:
                 return
--- feh_slideshow.py
+++ feh_slideshow.py
@@ -35,21 +35,36 @@ class App(tk.Frame):
         
         #make a field for the options buttons
         self.options_field = tk.Label(self, width = 40,bd = 2,relief='solid')
+        
+        #put the options field on the left 
         self.options_field.pack(side="left")
+
+        # options are dicts so the need to be sorted
         n = list(self.options.options.keys())
         n.sort()
+
+        # go through the options and create the check box
         for key in n:
-            if 'btn_selectable' in self.options.options[key].keys(): #if the item has the option to be selectable
-                if self.options.options[key]['btn_selectable'] == True: #is selectable enabled?
+            
+            #If the option is something that can on/off selectable 
+            if 'btn_selectable' in self.options.options[key].keys(): 
+
+                # the the selectability enabled
+                if self.options.options[key]['btn_selectable'] == True:
+                    
+                    #create the button 
                     self.check_btns.append(tk.Checkbutton(self.options_field, 
                                                         variable = self.options.options[key]['activated'], 
                                                         onvalue = True,
                                                         offvalue = False,
                                                         text = self.options.options[key]['lang_name'],
                                                         font = ["Helvetica",20]))
+        
+        # pack all the button elements on top of each other and push them to the left
         for btn in self.check_btns:
             btn.pack(side = 'top', anchor = 'w')
         
+        # make the quit button
         self.quit = tk.Button(self)
         self.quit["text"] = "Luk"
         self.quit["fg"] = "red"
@@ -57,59 +72,116 @@ class App(tk.Frame):
         self.quit["command"] = root.destroy
         self.quit.pack(side="bottom")
         
+        # make the start slideshow button
         self.start_btn = tk.Button(self)
         self.start_btn["text"] = "Start slideshow"
         self.start_btn["font"] = ["Helvetica",20]
         self.start_btn["command"] = self.start_slide
         self.start_btn.pack(side="bottom")
 
+        # container for the delay input and some text
         self.delay_field = tk.Label(self,width = 40)
         self.delay_field.pack(side = 'bottom')
 
-        self.delay_text = tk.Label(master = self.delay_field, text = "delay in sec.")
+        # make the text that says this is the input field for the delay
+        self.delay_text = tk.Label(master = self.delay_field, text = "skiftetid i sek.")
         self.delay_text.configure(font = ["Helvetica",20], padx = 5 )
         self.delay_text.pack(side = "left")
 
+        # This is the entry field for the delay. It is badly documented how the text validation works,
+        # but the vcmd is a function that can carry the following information to the validation function.
         # %s = value of entry prior to editing
         # %S = the text string being inserted or deleted, if any
         # %d = Type of action (1=insert, 0=delete, -1 for others)
         # %V = the type of validation that triggered the callback
         #      (key, focusin, focusout, forced)
         vcmd = (self.register(self.validate_delay),'%d', '%s','%S','%V')
+
+        # create the text entry field for the slideshow delay
         self.delay_entry = tk.Entry(master = self.delay_field, width = 3,)
         self.delay_entry.configure(font = ["Helvetica",20])
         self.delay_entry.insert(0,'7')
+        # try to validate the input on all possible inputs ie focus in/out, key typed/deleted
         self.delay_entry.configure(validate = 'all',vcmd = vcmd)
         self.delay_entry.pack(side = 'right')
     
     def validate_delay(self,d,s,S,V):
+        """Text validation function for the slideshow delay entry field
+            it makes sure that onyl numbers are typed and it does not go
+            less then 3 sec and more than 999 sec.
+        
+        Arguments:
+            d {str} -- %d = Type of action (1=insert, 0=delete, -1 for others)
+            s {str} -- %s = value of entry prior to editing
+            S {str} -- %S = the text string being inserted or deleted, if any
+            V {str} -- %V = the type of validation that triggered the callback
+                         (key, focusin, focusout, forced)
+
+        
+        Returns:
+             bool -- is the input valid
+        """
+
         try:
+            # Value prior to editing + the inserted char.
+            # Try to convert it to a float
+            # if it cannot convert to float the input cannot be a number
+            # if error the function returns false
             var = float(s+S)
-            if V == 'focusout' and var < 3:  #if too small delay
-                self.delay_entry.delete(0,'end') #delete the entry 
-                self.delay_entry.insert(0,'3') #write 3 
+
+            # If the user focus away from the textfield check that the value is smaller than 3
+            if V == 'focusout' and var < 3:
+                # If smaller delet the entry and write 3 for 3 sec
+                self.delay_entry.delete(0,'end')    #delete the entry 
+                self.delay_entry.insert(0,'3')      #write 3
+                # Also update the option so it matches the input
                 self.options.set_option(name = 'delay', data = 3) 
-            if len(s) < 3 or d == '0': #if entry is short enough or a delete 
+
+            # if the entry is less than 3 char or if user delets a char   
+            if len(s) < 3 or d == '0': 
+                
+                # if deleting a char
                 if d == '0':
-                    self.options.set_option(name = 'delay', data = s[:-1]) # return all but the last char
+                    # Update the option with the current typed in value minus the
+                    # last char becaus it's gonna be deleted.
+                    self.options.set_option(name = 'delay', data = s[:-1])
+                
+                # Else if the a char is inserted
                 else:
-                    self.options.set_option(name = 'delay', data = var)       
+                    # Update the option with the calculated float var
+                    self.options.set_option(name = 'delay', data = var)  
+
+                # Return true becaus it was a valid input because it could be 
+                # converted to a char and it was less than 3 or a delet char
                 return True 
             
         except ValueError:
+            # Return false because the input could not be converted to a float
             return False
+            
+        # If the function didn't return true anywhere it must have been a false input.     
         return False
 
 
     def browse_func(self,*arg):
+        """ Function that start the browsing of a folder
+        """
+        #set the path field as focus (may not be neccessary)
         self.path_field.focus_set()
+
+        # set the pathname
         self.path_name = tk.filedialog.askdirectory(title='vælg en billedmappe',initialdir = '/home/pi/Pictures')
-        self.path_name.rfind("/")
         
+        # find the folder name and display it (not implemented yet)
+        # self.path_name.rfind("/")
+        
+        # set the text of the path text box to the current path
         self.path_text["text"] = self.path_name
-        #self.pathlabel.config(text=filename)
+
     
     def start_slide(self):
+        """Function that starts feh with the commands contained in self
+        """
         #put the command together 
         command = 'feh '+ self.options.get_feh_args()+','+self.path_name
         
@@ -118,9 +190,13 @@ class App(tk.Frame):
         #call(command)
         print(command)
 
-        
+# create a root element for tkinter        
 root = tk.Tk()
+
+# create an instance of App class
 app = App(master=root)
+
+# Start the application
 app.mainloop()
 
 
